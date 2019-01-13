70f7aab9680c837345bb157734f5d99822c1d1cc
Paula Santiago <paula_GD19@hotmail.com>
2019-01-13 16:20:59 +0100

    Proyecto

---
+++ Proyecto/js/internal/Proyecto.js
@@ -0,0 +1,32 @@
+// Función barras skills 
+
+function initializeBars(barNameArray, barValueArray)
+  {
+	for(i = 0; i < barNameArray.length; i++ )
+    {
+      setBarValue(barNameArray[i], barValueArray[i]);
+    }	
+  }
+
+
+function setBarValue(barName, barValue) 
+  {
+    var elem = document.getElementById(barName);
+    var width = 1;
+    var id = setInterval(frame, 10);
+  
+    function frame() 
+      {
+        if (width >= barValue) 
+        {
+        clearInterval(id);
+        }
+       else 
+        {
+        width++;
+        elem.style.width = width + "%"; 
+        elem.innerHTML = width * 1 + "%"
+        }
+      }
+  }
+
