7bc02e5d1b6aa74939378ca8530cb33dbfaa7ee8
Paula Santiago <paula_GD19@hotmail.com>
2019-02-01 09:28:27 +0100

    TypeScript e interfaz gestor portfolio

---
+++ 10_TypeScript/01_variables_y_fun.js
@@ -0,0 +1,7 @@
+var unNumero;
+unNumero = 12;
+// unNumero = "un texto"; No se podría poner porque ya hemos dicho que es un número
+console.log("Un número " + unNumero);
+var unTexto = "Un texto";
+console.log(unTexto);
+alert(unTexto);
---
+++ 10_TypeScript/01_variables_y_fun.ts
@@ -0,0 +1,9 @@
+let unNumero: number; 
+
+unNumero = 12; 
+// unNumero = "un texto"; No se podría poner porque ya hemos dicho que es un número
+console.log("Un número " + unNumero);
+
+var unTexto = "Un texto"; 
+console.log(unTexto); 
+alert(unTexto); 
